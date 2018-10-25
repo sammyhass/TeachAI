d6ae2ed1b2eb4c88165766e5b7e3bc96a8890888
Sammy Hass <sammy.hass@outlook.com>
2018-10-25 16:40:49 +0100

    Created TeachAI and LinearRegressor

---
+++ LinearRegressor.py
@@ -0,0 +1,25 @@
+class LinearRegressor:
+	def __repr__(self):
+		return "TeachAI.LinearRegressor()"
+
+	def fit(self, xs, ys):
+		sum_x = 0
+		sum_y = 0
+		if len(xs) != len(ys):	
+			raise Exception("Number of xs must be the same as number of ys")
+		for i in range(len(xs)):
+			sum_x += xs[i]
+			sum_y += ys[i]
+
+		mean_x = sum_x / len(xs)
+		mean_y = sum_y / len(ys)
+
+		num = 0
+		den = 0
+		for i in range(len(xs)):
+			num += (xs[i] - mean_x) * (ys[i] - mean_y)
+			den += (xs[i] - mean_x) * (xs[i]- mean_x)
+
+		m = num / den
+		b = mean_y - m * mean_x
+		return [m, b]
---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .LinearRegressor import LinearRegressor
