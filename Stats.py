08dafd6a20daa53912f2831eba2817e8da4bfb13
Sammy Hass <sammy.hass@outlook.com>
2018-10-27 15:24:30 +0100

    Added max and argmax fn

--- Stats.py
+++ Stats.py
@@ -11,3 +11,18 @@ def std(xs):
 	for i in range(len(xs)):
 		total += (xs[i] - av)*(xs[i] - av)
 	return math.sqrt(1/(len(xs))* total)
+
+def max(xs):
+	maxi = xs[0]
+	for i in range(len(xs)):
+		if xs[i] > maxi:
+			maxi = xs[i]
+	return maxi
+
+def argmax(xs):
+	maxi = 0
+	for i in range(len(xs)):
+		if xs[i] > maxi:
+			maxi = i
+	return maxi
+
