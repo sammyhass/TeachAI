55757910f47e896866d8471e244d9220a74782d1
Sammy Hass <sammy.hass@outlook.com>
2018-10-29 21:30:05 +0000

    Updated pypi info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library for teaching AI/ML',
-  long_description = 'Library for teaching AI/ML'   # Give a short description about your library
+  long_description = 'Library for teaching AI/ML',   # Give a short description about your library
   author = 'Sammy Hass',                   # Type in your name
   author_email = 'sammy.hass@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/SammyHass/TeachAI',   # Provide either the link to your github or to your website
