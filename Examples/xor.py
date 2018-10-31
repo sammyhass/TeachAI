46e7e440fe33bc8684240360de7f3de877579716
Sammy Hass <sammy.hass@outlook.com>
2018-10-31 12:18:31 +0000

    Update xor.py

--- Examples/xor.py
+++ Examples/xor.py
@@ -3,7 +3,7 @@ import TeachAI
 xs = [[1, 0], [1, 1], [0, 0], [0, 1]] # xs data for xor
 ys = [[1], [0], [0], [1]] # ys data for xor
 
-model = TeachAI.NeuralNetwork(2, 4, 1)
-model.fit(xs, ys, 1, 5)
-for x in xs:
+model = TeachAI.NeuralNetwork(2, 10, 1) # create neural net with architecture 2 input nodes, 10 hidden nodes and 1 output node
+model.fit(xs, ys, 1, 1000) # fit the model with learning rate of 1 and run over 1000 epochs
+for x in xs: 
 	print("Prediction for {}: {}".format(x, model.predict(x)))
