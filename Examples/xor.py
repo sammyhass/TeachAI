5f2c406183c489744821101b24a588ea77545ac5
Sammy Hass <sammy.hass@outlook.com>
2018-10-31 12:13:20 +0000

    Update xor.py

--- Examples/xor.py
+++ Examples/xor.py
@@ -4,7 +4,6 @@ xs = [[1, 0], [1, 1], [0, 0], [0, 1]] # xs data for xor
 ys = [[1], [0], [0], [1]] # ys data for xor
 
 model = TeachAI.NeuralNetwork(2, 4, 1)
-model.fit(xs, ys, 0.01, 50000)
-
+model.fit(xs, ys, 1, 5)
 for x in xs:
 	print("Prediction for {}: {}".format(x, model.predict(x)))
